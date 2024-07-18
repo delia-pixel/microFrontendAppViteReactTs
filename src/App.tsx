18317ae9c3bcbc3586a3fe1c27ea01b33da484db
delia-pixel <tpatriciareinedia@gmail.com>
2024-07-18 21:01:44 +0100

    suite

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
+import { Link } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
-import MoovieItem from "./components/MoovieItem";
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="flex gap-4">
-        <MoovieItem
-          image="/iADOJ8Zymht2JPMoy3R7xceZprc.jpg"
-          title={"Game of thrones"}
-          date={"20 April"}
-          age={12}
-        />
-        <MoovieItem
-          image="/iADOJ8Zymht2JPMoy3R7xceZprc.jpg"
-          title={"Game of thrones"}
-          date={"20 April"}
-          age={12}
-        />
-        <MoovieItem
-          image="/iADOJ8Zymht2JPMoy3R7xceZprc.jpg"
-          title={"Game of thrones"}
-          date={"20 April"}
-          age={12}
-        />
-        <MoovieItem
-          image="/iADOJ8Zymht2JPMoy3R7xceZprc.jpg"
-          title={"Game of thrones"}
-          date={"20 April"}
-          age={12}
-        />
-      </div>
+      <h1 className="text-4xl text-center">Remote app</h1>
+      <Link to="/details">Details</Link>
     </>
   );
 }
